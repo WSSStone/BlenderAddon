98b6a42ca8f0dddba535d33cca08486daa78ec65
WSSStone <497475775@qq.com>
2025-01-08 11:20:17 +0800

    modify: defaultly publish all extensions without checking whether it is new

--- publish_extension.py
+++ publish_extension.py
@@ -7,16 +7,23 @@ import git
 class extension_publisher:
     def __init__(self):
         self.config = configparser.ConfigParser()
+
         self.config.read('config.ini')
+
         self.BLENDER = self.config['Paths']['blender_path']
+
         self.SRC_DIR = self.config['Paths']['src_dir']
+
         self.DST_DIR = self.config['Paths']['dst_dir']
-        self.changed_extensions = {}
+
+        self.publications = {}
+
 
     def build(self, ext:str):
         cwd = os.getcwd()
 
         src = os.path.join(self.SRC_DIR, ext)
+
         dst = os.path.join(self.DST_DIR, ext)
 
         os.chdir(src)
@@ -29,24 +36,37 @@ class extension_publisher:
 
         os.chdir(cwd)
 
+
     def publish(self):    
-        for ext in self.changed_extensions:
+        for ext in self.publications:
             self.build(ext)            
 
     def cleanup(self):
-        for ext in self.changed_extensions:
+        for ext in self.publications:
             p = os.path.join(self.DST_DIR, f'{ext}-1.0.0-windows_x64.zip')
+
             if os.path.exists(p):
                 os.remove(p)
+
 
     def check_changes(self):
         repo = git.Repo(os.getcwd())
 
         diff = repo.index.diff(None)
+
         for item in diff:
             if 'src/' in item.a_path:
                 ext = item.a_path.split('/')[1]
-                self.changed_extensions[ext] = True
+                self.publications[ext] = True
+
+    def alloc_publications(self, check_changes=False):
+        if check_changes:
+            self.check_changes()
+        else:
+            for ext in os.listdir(self.SRC_DIR):
+                if os.isdir(os.path.join(self.SRC_DIR, ext)):
+                    self.publications[ext] = True
+
 
     def check_dependency(self):
         if not os.path.exists(f'{self.BLENDER}'):
@@ -55,42 +75,48 @@ class extension_publisher:
 
         if not os.path.exists(self.SRC_DIR):
             raise Exception("Source directory lost. Repull repo.")
-        
+
         os.makedirs(self.DST_DIR, exist_ok=True)
+
 
     def validate_manifest(self):
         cwd = os.getcwd()
 
-        for ext in self.changed_extensions:
+        for ext in self.publications:
             src = os.path.join(self.SRC_DIR, ext)
             os.chdir(src)
 
             cmd = f'{self.BLENDER} --command extension validate'
 
             p = subprocess.Popen(cmd, shell=True, cwd=os.getcwd())
+
             p.wait()
 
             os.chdir(cwd)
 
+
     def validate_zip(self):
         cwd = os.getcwd()
-        for ext in self.changed_extensions:
+
+        for ext in self.publications:
             dst = os.path.join(self.DST_DIR)
             os.chdir(dst)
 
             cmd = f'{self.BLENDER} --command extension validate {ext}-1.0.0-windows_x64.zip'
 
             p = subprocess.Popen(cmd, shell=True, cwd=os.getcwd())
+
             p.wait()
 
             os.chdir(cwd)
+
 
     def run(self):
         self.check_dependency()
 
         self.validate_manifest()
 
-        self.check_changes()
+        self.alloc_publications(check_changes=False)
 
         self.cleanup()
 
